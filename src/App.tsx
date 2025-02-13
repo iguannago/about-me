bed369bb867efaa22a882362b8cf6110302f92ad
David Crespo <david.crespo@e-asolutions.com>
2025-02-13 11:24:33 +0000

    add ExpenseTracker component to manage and display expenses

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import MainContent from './components/MainContent';
 import './App.css';
 import Footer from './components/Footer';
+import ExpenseTracker from './components/ExpenseTracker';
 
 const App: React.FC = () => {
   return (
     <div className='app-container'>
       <MainContent />
+      <ExpenseTracker />
       <Footer />
     </div>
   );
